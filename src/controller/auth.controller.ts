6a39aff0959edfa13ce60ac2249c380fe9fc6a98
CrSjimo <crs_16423@outlook.com>
2025-05-07 12:11:26 +0800

    feat: 鉴权相关更新

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,15 +1,31 @@
 import { Request, Response } from 'express'
 import bcrypt from 'bcrypt'
+import validator from 'validator'
 import { PrismaClient } from '@prisma/client'
 import { sendVerificationEmail } from '../service/mail.service'
-import { generateResetToken } from '../service/token.service'
+import { generateAccessToken, generateVerifyToken } from '../service/token.service'
 import { ResponseError } from '../util/errors'
 
 const prisma = new PrismaClient()
 const SALT_ROUNDS = 10
 
-export const register = async (req: Request, res: Response) => {
+export async function register(req: Request, res: Response) {
     const { email, password } = req.body
+
+    const verify = () => {
+        if (typeof(email) !== 'string')
+            return false
+        if (typeof(password) !== 'string')
+            return false
+        if (!validator.isEmail(email))
+            return false
+        if (!validator.isLength(password, { min: 6 }))
+            return false
+        return true
+    }
+    if (!verify) {
+        throw new ResponseError(400, 'Invalid request')
+    }
     
     const existingUser = await prisma.user.findUnique({ where: { email } })
     if (existingUser) {
@@ -21,9 +37,37 @@ export const register = async (req: Request, res: Response) => {
         data: { email, password: hashedPassword }
     })
 
-    const token = generateResetToken(user.id)
+    const token = generateVerifyToken(user.id)
     await sendVerificationEmail(email, token)
 
     return res.status(204).send()
-    
+
+}
+
+export async function login(req: Request, res: Response) {
+    const { email, password } = req.body
+
+    const verify = () => {
+        if (typeof(email) !== 'string')
+            return false
+        if (typeof(password) !== 'string')
+            return false
+        if (!validator.isEmail(email))
+            return false
+        if (!validator.isLength(password, { min: 6 }))
+            return false
+        return true
+    }
+    if (!verify) {
+        throw new ResponseError(400, 'Invalid request')
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+    const user = await prisma.user.findUnique({ where: { email, password: hashedPassword, isVerified: true } })
+    if (!user) {
+        throw new ResponseError(403, 'Cannot login')
+    }
+
+    const token = generateAccessToken(user.id, user.password)
+    return res.status(200).json({ token, id: user.id })
 }
--- src/service/token.service.ts
+++ src/service/token.service.ts
@@ -1,9 +1,13 @@
 import jwt from 'jsonwebtoken'
 
-export const generateAccessToken = (userId: number, password: string) => {
+export function generateAccessToken(userId: string, password: string) {
     return jwt.sign({ sub: userId, pwd: password }, process.env.JWT_SECRET!, { expiresIn: '100d' })
 };
 
-export const generateResetToken = (userId: number) => {
-    return jwt.sign({ sub: userId }, process.env.JWT_SECRET!, { expiresIn: '1h' })
+export function generateVerifyToken(userId: string) {
+    return jwt.sign({ sub: userId, verify: true }, process.env.JWT_SECRET!, { expiresIn: '1h' })
+};
+
+export function generateUpdateEmailToken(userId: string, newEmail: string) {
+    return jwt.sign({ sub: userId, email: newEmail }, process.env.JWT_SECRET!, { expiresIn: '1h' })
 };
