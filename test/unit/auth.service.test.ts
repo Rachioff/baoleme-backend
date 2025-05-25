fc42db97ffb1d1886611040bac38128fb65f98c1
CrSjimo <crs_16423@outlook.com>
2025-05-25 17:00:04 +0800

    feat: 添加了测试功能

---
+++ test/unit/auth.service.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test, jest } from '@jest/globals'
+
+describe('auth service', () => {
+    test('should register a user', async () => {
+        expect(true).toBe(true)
+    })
+})
